39f8cb4e0cca7b329e3c8dbef1234227cd797bf5
Sambou Diao <sam.diao90@gmail.com>
2026-05-04 17:18:02 +0200

    Add monitoring

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from app.rag import search_docs, generate_answer
 from app.logger import log_query
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
-
+Instrumentator().instrument(app).expose(app) # monitoring prometheus
 class Query(BaseModel):
     question: str
 
